7b187d254d1dd009a035ec978b11513b94e1efdb
Dominique <Dominique@DESKTOP-LK5SI4Q>
2019-04-10 22:15:29 -0500

    dropdown

--- src/main/resources/static/js/johnShop.js
+++ src/main/resources/static/js/johnShop.js
@@ -1,8 +1,22 @@
 var myApp = angular.module('shop',[]);
 
 myApp.controller('Controller',  function($scope,$http,$window,$interval) {
- 
-	
+		$scope.choice=
+			{
+				Name:''
+			};
+		$scope.chosen;
+		$scope.user=
+			{	stu_ID:'',
+				Name: '',
+				Location:'',
+				snack:''
+			};
+		
+		$scope.tr = $scope.choice.name;
+		 
+		
+		
 	$window.onload = function () {$http({
 		  method:'GET',
 			  url:'/api/Snack/all'})
@@ -10,8 +24,20 @@ myApp.controller('Controller',  function($scope,$http,$window,$interval) {
 			  {
 			  	$scope.snacks= response.data;
 			  	console.log($scope.snacks);
-			  	console.log($scope.checklist);
+			  	
 			  })};
 			  $scope.chosen;
 			  $scope.choice;
+	
+			  $scope.add = function (){$http({
+					method:'POST',
+				    url:'/api/request/add',
+				    
+						data: $scope.user
+					
+							}).then(function(response)
+									{	
+										console.log($scope.user);
+										
+										})};
 });
